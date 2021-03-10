3910991714319cfc21c424878b5561e52d0632b7
James Boyette <james.boyette@gmail.com>
2021-03-09 19:23:49 -0500

    Add All Code Practice Files

---
+++ 3_Methods_Practice/Methods_Practice.rb
@@ -0,0 +1,34 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+# # solicit user input for a string
+# p "Hey! Give me a word, please!"
+# # store that input in a variable
+# word = gets.chomp
+# # run .upcase on that variable and print
+# p "Did you say #{word.upcase}?!"
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# solicit a number from the user
+
+# store that number in a variable
+
+#
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+# SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
