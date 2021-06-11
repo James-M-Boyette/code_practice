6a51ae7c66c151478fc96b06ca6165ee04ce07f0
James Boyette <james.boyette@gmail.com>
2021-06-11 15:02:06 -0400

    Add Functions practice

--- 7_Functions_Practice/Functions_practice.rb
+++ 7_Functions_Practice/Functions_practice.rb
@@ -1,21 +1,94 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
+# def number(n)
+#   p n
+#   p n
+# end
+
+# number(4)
+
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+# def capitalize(string)
+#   p string.upcase
+# end
+
+# capitalize("hello - can you hear me?")
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
+# def subtract(number1, number2)
+#   difference = number1 - number2
+#   difference
+#   p difference
+# end
+
+# subtract(7, 3)
+
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+# def square(number)
+#   equals = number * number
+#   equals
+#   p equals
+# end
+
+# square(4)
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
+# def first_letter(sentance)
+#   # p sentance[0]
+#   return sentance[0]
+# end
+
+# f_l = first_letter("Here's the sentance")
+# p f_l
+
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+# def combo(string1, string2, string3)
+#   string_combo = string1 + " " + string2 + " " + string3
+# end
+
+# best_song_ever = combo("Hi!", "My name is-", "Hi! My name is ...")
+# p best_song_ever
 
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
+# def digit_to_string(number)
+#   return number.to_s
+# end
+
+# string = digit_to_string(34)
+# p string
+# p string.class
+
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+# def times5(string)
+#   return string, string, string, string, string
+# end
+
+# repeated = times5("Hi, my name is-")
+# p repeated
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
+# def average(number1, number2, number3)
+#   avg = (number1 + number2 + number3) / 3
+# end
+
+# avg = average(4, 8, 12)
+# p avg
+
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+# def hundred30(number)
+#   return number * 10 + 30
+# end
+
+# result = hundred30(6)
+# p result
 
 # SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
