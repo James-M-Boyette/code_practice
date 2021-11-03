63083d8e1057b684b56a7c96ac3119f469d4bd4d
sou7hernsaint <sou7hernsaint@gmail.com>
2021-11-03 05:50:37 -0400

    First Addition

--- 11_Converting_Data_Practice/Converting_Data_Practice.rb
+++ 11_Converting_Data_Practice/Converting_Data_Practice.rb
@@ -1,6 +1,18 @@
 #  1. Convert an array of arrays into a hash.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
+input = [[1, 3], [8, 9], [2, 16]]
+
+def array_to_hash(array)
+  result = hash[*[array]]
+  result
+end
+
+output = array_to_hash(input)
+p output
+
+# output should be an hash, with the first value turned into the key, and the seonc turned into the value {1: 3, 8: 9, 2: 16}
+
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
--- 1_Conditionals_Practice/Conditionals_Practice.rb
+++ 1_Conditionals_Practice/Conditionals_Practice.rb
@@ -1,11 +1,12 @@
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-def (n)
-var = 
+print "test"
+var = 15
+
 if var == 10
-  p 0
-else
-  p -1
+  print 0
+else 
+  print -1
 end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
