63fad8e2661dfd3a12fe9d72afa34d2132e8c5dd
sou7hernsaint <sou7hernsaint@gmail.com>
2021-11-15 15:55:40 -0500

    First Refresher, using Ruby & Conditionals

---
+++ 1_Conditionals_Practice/21_11_15_(Ruby)Conditionals_Practice.rb
@@ -0,0 +1,358 @@
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+
+
+# variable = 10
+
+
+
+# if variable == 10
+
+
+
+#   p "0"
+
+
+
+# else
+
+
+
+#   p "-1"
+
+
+
+# end
+
+
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+
+
+# variable = 10.1
+
+
+
+# if variable < 10
+
+
+
+#   p -1
+
+
+
+# elsif variable > 10
+
+
+
+#   p 1
+
+
+
+# elsif variable == 10
+
+
+
+#   p 0
+
+
+
+# else
+
+
+
+#   p "something may be wrong"
+
+
+
+# end
+
+
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+
+
+# var1 = 4
+
+
+
+# var2 = 9
+
+
+
+# if var1 < 10 && var2 < 10
+
+
+
+#   p 1
+
+
+
+# else
+
+
+
+#   p 0
+
+
+
+# end
+
+
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+
+
+# var = 1000
+
+
+
+# if var > 9000
+
+
+
+#   p 1
+
+
+
+# else
+
+
+
+#   p -1
+
+
+
+# end
+
+
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+
+
+var = 38
+
+
+
+# if var < 10
+
+
+
+#   p 9
+
+
+
+# elsif var < 20
+
+
+
+#   p 19
+
+
+
+# elsif var < 30
+
+
+
+#   p 29
+
+
+
+# else
+
+
+
+#   p -1
+
+
+
+# end
+
+
+
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+
+
+# var1 = 9
+
+
+
+# var2 = 8
+
+
+
+# if var1 > 10 || var2 > 10
+
+
+
+#   p 100
+
+
+
+# else
+
+
+
+#   p -100
+
+
+
+# end
+
+
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+
+
+# var = -1777
+
+
+
+# if var < 0
+
+
+
+#   p 1776
+
+
+
+# else
+
+
+
+#   p 1979
+
+
+
+# end
+
+
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+
+
+# var = 99
+
+
+
+# if var == 100
+
+
+
+#   p 100
+
+
+
+# elsif var == 99
+
+
+
+#   p 99
+
+
+
+# else
+
+
+
+#   p 0
+
+
+
+# end
+
+
+
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+
+
+# var1 = -1
+
+
+
+# var2 = 2
+
+
+
+# if var1 < 0 && var2 > 0
+
+
+
+#   p 1
+
+
+
+# else
+
+
+
+#   p 0
+
+
+
+# end
+
+
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+
+
+# var = -21
+
+
+
+# if var > 80
+
+
+
+#   p 5
+
+
+
+# elsif var > 60
+
+
+
+#   p 4
+
+
+
+# elsif var > 40
+
+
+
+#   p 3
+
+
+
+# elsif var > 20
+
+
+
+#   p 2
+
+
+
+# else
+
+
+
+#   p 1
+
+
+
+# end
+
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
+
