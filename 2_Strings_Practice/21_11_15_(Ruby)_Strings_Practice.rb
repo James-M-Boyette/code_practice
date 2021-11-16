901586bccdfc928f7ad2cae9d3eb0eab9c7ef0e6
sou7hernsaint <James.M.84.Boyette@gmail.com>
2021-11-15 19:08:46 -0500

    Create template file (for future practice sessions) + complete Ruby DDP practice

---
+++ 2_Strings_Practice/21_11_15_(Ruby)_Strings_Practice.rb
@@ -0,0 +1,274 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+
+
+# var1 = "James"
+
+
+
+# var2 = "Boyette"
+
+
+
+# p var1 + " " + var2
+
+
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+
+
+# var1 = "Jess"
+
+
+
+# var2 = "Boyette"
+
+
+
+# p "#{var1} #{var2}"
+
+
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+
+
+# p "Hello user! Please tell me a word!"
+
+
+
+# var = gets.chomp
+
+
+
+# if var == "marco"
+
+
+
+#   p "polo"
+
+
+
+# end
+
+
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+
+
+# var1 = "blue"
+
+
+
+# var2 = "red"
+
+
+
+# var3 = "green"
+
+
+
+# p "My favorite colors right now are " + var1 + ", " + var2 + ", and" + var3
+
+
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+
+
+# var1 = "blue"
+
+
+
+# var2 = "red"
+
+
+
+# var3 = "green"
+
+
+
+# p "My favorite colors right now are #{var1}, #{var2}, and #{var3}!"
+
+
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+
+
+# p "Please tell me the name of a person you really like:"
+
+
+
+# var = gets.chomp
+
+
+
+# if var != "Santa"
+
+
+
+#   p "You're not Santa!"
+
+
+
+# end
+
+
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+
+
+# p "Please tell me three of your favorite book titles (seperated by commas)!"
+
+
+
+# var = gets.chomp
+
+
+
+# var = var.split(",")
+
+
+
+# p var
+
+
+
+# p var[0]
+
+
+
+# var1 = var[0]
+
+
+
+# var2 = var[1]
+
+
+
+# var3 = var[2]
+
+
+
+# p "Ok - so you've picked " + var1 + ", " + var2 + ", and " + var3 + ", correct?"
+
+
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+
+
+# p "Please tell me three of your favorite book titles (seperated by commas)!"
+
+
+
+# var = gets.chomp
+
+
+
+# var = var.split(",")
+
+
+
+# p var
+
+
+
+# p var[0]
+
+
+
+# var1 = var[0]
+
+
+
+# var2 = var[1]
+
+
+
+# var3 = var[2]
+
+
+
+# p "Ok - so you've picked #{var1}, #{var2}, and #{var3}, correct?"
+
+
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+
+
+# p "Please give me a password to secure your profile:"
+
+
+
+# var = gets.chomp
+
+
+
+# if var == "Joshua"
+
+
+
+#   p "Shall we play a game?"
+
+
+
+# else
+
+
+
+#   p "Access denied"
+
+
+
+# end
+
+
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+
+
+# p "Please tell me three cities you'd *love* to travel to (seperated by commas)!"
+
+
+
+# var = gets.chomp
+
+
+
+# var = var.split(",")
+
+
+
+# p var
+
+
+
+# p var[0]
+
+
+
+# var1 = var[0]
+
+
+
+# var2 = var[1]
+
+
+
+# var3 = var[2]
+
+
+
+# p "Ok - so you've picked " + var1 + ", " + var2 + ", and " + var3 + ", correct?"
+
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
+
