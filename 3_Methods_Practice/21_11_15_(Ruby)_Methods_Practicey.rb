09e962f42786ac76240ed98a22e713b7467fc5b9
sou7hernsaint <James.M.84.Boyette@gmail.com>
2021-11-17 20:51:24 -0500

    Add Methods DDP for Ruby

--- 3_Methods_Practice/21_11_15_(Ruby)_Methods_Practicey.rb
+++ 3_Methods_Practice/21_11_15_(Ruby)_Methods_Practicey.rb
@@ -18,43 +18,75 @@
 
 
 
-p "Please give me an whole number - any number other than zero!"
+# p "Please give me an whole number - any number other than zero!"
 
 
 
-var = gets.chomp
+# var = gets.chomp
 
 
 
-var2 = var.to_i
+# var2 = var.to_i
 
 
 
-if var2 == 0
-
-  p "I'm afraid that's either not a number, or not a zero"
-
-else
-
-  p "Nice pick, #{var2}"
+# if var2 == 0
 
 
 
-  if var2 > 100
+#   p "I'm afraid that's either not a number, or not a zero"
 
-    p "In fact, that's a bIg number!"
 
-  else
 
-    p "Although, I thought you'd tell me something higher ..."
+# else
 
-  end
 
-end
+
+#   p "Nice pick, #{var2}"
+
+
+
+#   if var2 > 100
+
+
+
+#     p "In fact, that's a bIg number!"
+
+
+
+#   else
+
+
+
+#     p "Although, I thought you'd tell me something higher ..."
+
+
+
+#   end
 
 
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+
+
+# p "Please give me a number:"
+
+
+
+# var1 = gets.chomp.to_i
+
+
+
+# p "Please give me second number:"
+
+
+
+# var2 = gets.chomp.to_i
+
+
+
+# p var1 + var2
 
 
 
@@ -78,7 +110,31 @@ end
 
 
 
+# p "Please tell me a word, and let's see if I can reverse it:"
+
+
+
+# var = gets.chomp
+
+
+
+# p "#{var.reverse}"
+
+
+
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+
+
+# p "Please give me a number, and I'll see if I can make it ten times bigger:"
+
+
+
+# var = gets.chomp
+
+
+
+# p "#{var.to_i * 10}"
 
 
 
@@ -86,7 +142,35 @@ end
 
 
 
+# p "Please give me two words, seperated by a comma:"
+
+
+
+# var = gets.chomp.split(",")
+
+
+
+# p "#{var[0].upcase} #{var[1].upcase}!"
+
+
+
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+
+
+# p "Please give me a word and I'll tell you how many letters are in that word:"
+
+
+
+# var = gets.chomp
+
+
+
+# p "#{var}"
+
+
+
+# p "Your word has #{var.size} letters"
 
 
 
@@ -94,11 +178,95 @@ end
 
 
 
+# p "Please give me a number, and I'll tell you whether it's a negative or positive one:"
+
+
+
+# var = gets.chomp
+
+
+
+# if var.to_i < 0
+
+
+
+#   p "That's a NEGATIVE number"
+
+
+
+# elsif var.to_i > 0
+
+
+
+#   p "That's a POSITIVE number"
+
+
+
+# else
+
+
+
+#   p "That's ZERO!"
+
+
+
+# end
+
+
+
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
 
 
+# p "Please tell me two numbers and I'll multiply them"
+
+
+
+# var = gets.chomp.split
+
+
+
+# p var[0]
+
+
+
+# p var[1]
+
+
+
+# p var[0].to_i * var[1].to_i
+
+
+
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+
+
+# p "Please tell me a word and we'll see if it's long:"
+
+
+
+# var = gets.chomp
+
+
+
+# if var.size > 5
+
+
+
+#   p "That's a long word!"
+
+
+
+# else
+
+
+
+#   p "Not a particularly-big word"
+
+
+
+# end
 
 
 
