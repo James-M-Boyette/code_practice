996874ff3aeee0574b70c073983ce44792f0d076
sou7hernsaint <James.M.84.Boyette@gmail.com>
2021-11-29 17:36:10 -0500

    Complete Ruby practice

--- 7_Functions_Practice/21_11_29_(Ruby)_Functions_practice.rb
+++ 7_Functions_Practice/21_11_29_(Ruby)_Functions_practice.rb
@@ -2,7 +2,55 @@
 
 
 
+# def double(input)
+
+
+
+#   result = input * 2
+
+
+
+#   p result
+
+
+
+#   return result
+
+
+
+# end
+
+
+
+# double(2)
+
+
+
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+
+
+# def yell(input)
+
+
+
+#   big = input.upcase
+
+
+
+#   p big
+
+
+
+#   return big
+
+
+
+# end
+
+
+
+# yell("Hello!")
 
 
 
@@ -10,7 +58,55 @@
 
 
 
+# def subtraction(inputOne, inputTwo)
+
+
+
+#   result = inputOne - inputTwo
+
+
+
+#   p result
+
+
+
+#   return result
+
+
+
+# end
+
+
+
+# subtraction(5, 10)
+
+
+
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+
+
+# def squared(input)
+
+
+
+#   result = input ** 2
+
+
+
+#   p result
+
+
+
+#   return result
+
+
+
+# end
+
+
+
+# squared(5)
 
 
 
@@ -18,7 +114,51 @@
 
 
 
+# def firstLetter(input)
+
+
+
+#   result = input[0]
+
+
+
+#   p result
+
+
+
+#   return result
+
+
+
+# end
+
+
+
+# firstLetter("Zupercalifragilistic")
+
+
+
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+
+
+# def smash(inputOne, inputTwo, inputThree)
+
+
+
+#   result = inputOne + " " + inputTwo + " " + inputThree
+
+
+
+#   p result
+
+
+
+# end
+
+
+
+# smash("Here", "I", "am")
 
 
 
@@ -26,7 +166,59 @@
 
 
 
+# def toString(input)
+
+
+
+#   result = input.to_s
+
+
+
+#   p result
+
+
+
+#   p result.class
+
+
+
+# end
+
+
+
+# toString(666)
+
+
+
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+
+
+# def repeater(input)
+
+
+
+#   result = (input.to_s + " ") * 5
+
+
+
+#   result = result.split
+
+
+
+#   p result
+
+
+
+#   p result.class
+
+
+
+# end
+
+
+
+# repeater("Hi!")
 
 
 
@@ -34,7 +226,47 @@
 
 
 
+# def average(inputOne, inputTwo, inputThree)
+
+
+
+#   result = (inputOne + inputTwo + inputThree) / 3
+
+
+
+#   p result
+
+
+
+# end
+
+
+
+# average(10, 25, 50)
+
+
+
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+
+
+# def upSequencer(input)
+
+
+
+#   result = (input * 10) + 30
+
+
+
+#   p result
+
+
+
+# end
+
+
+
+# upSequencer(5)
 
 
 
