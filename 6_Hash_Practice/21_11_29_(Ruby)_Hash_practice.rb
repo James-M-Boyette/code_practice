88da85fcf69209dffdd8fbdbdb71d70dfb90cba6
sou7hernsaint <James.M.84.Boyette@gmail.com>
2021-11-29 17:16:19 -0500

    Complete Ruby Hash practice

--- 6_Hash_Practice/21_11_29_(Ruby)_Hash_practice.rb
+++ 6_Hash_Practice/21_11_29_(Ruby)_Hash_practice.rb
@@ -2,7 +2,31 @@
 
 
 
+# hash = { "first": "James", "last": "Boyette", "email": "myemail@gmail.com" }
+
+
+
+# hash.each do |e|
+
+
+
+#   p e
+
+
+
+# end
+
+
+
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+
+
+# users = [{ "first": "James", "last": "Boyette", "email": "myemail@gmail.com" }, { "first": "Jessica", "last": "Pendlton", "email": "myJemail@gmail.com" }, { "first": "John", "last": "Witchitah", "email": "myJWemail@gmail.com" }]
+
+
+
+# p users[0]
 
 
 
@@ -10,7 +34,39 @@
 
 
 
+# inventory = [{ :item => "Air Jordans", :price => 120 }, { "item": "LG TV", :price => 1500 }, { :item => "Christmas Decoration", :price => 5 }]
+
+
+
+# inventory << { :item => "Thanksgiving Turkey", :price => 13 }
+
+
+
+# p inventory
+
+
+
+# p inventory[3][:item]
+
+
+
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+
+
+# favoriteBook = { :title => "Hitchhiker's #{'"Guide"'} to the Galaxy", :author => "Douglas Adams", :pageCount => 208, :originalLanguage => "English" }
+
+
+
+# favoriteBook.each do |element|
+
+
+
+#   pp element
+
+
+
+# end
 
 
 
@@ -18,7 +74,39 @@
 
 
 
+# bookShelf = [{ :title => "Hitchhiker's #{'"Guide"'} to the Galaxy", :author => "Douglas Adams", :pageCount => 208, :originalLanguage => "English" },
+
+
+
+#              { :title => "The First 90 Days", :author => "Michael D. Watkins", :pageCount => 304, :originalLanguage => "English" },
+
+
+
+#              { :title => "Gordon Ramsay's Ultimate Cookery Course", :author => "Gordon Ramsay", :pageCount => 123, :originalLanguage => "English" }]
+
+
+
+# p bookShelf[2][:author]
+
+
+
 # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+
+
+# stateCapitals = { :California => "Sacramento", :New_York => "Albany", :Alabama => "Montgomery" }
+
+
+
+# p stateCapitals
+
+
+
+# stateCapitals[:Arizona] = "Phoenix"
+
+
+
+# p stateCapitals
 
 
 
@@ -26,7 +114,47 @@
 
 
 
+# dreamLaptop = { :brand => "Toshiba", :model => "2022 SuperComputer", :year => 2022 }
+
+
+
+# dreamLaptop.each do |element|
+
+
+
+#   p element
+
+
+
+# end
+
+
+
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+
+
+# laptopOptions = [
+
+
+
+#   { :brand => "Dell", :model => "Dude!", :year => 2007 },
+
+
+
+#   { :brand => "AlienWare", :model => "2021 Overpriced", :year => 2021 },
+
+
+
+#   { :brand => "Gateway", :model => "2016 Blah", :year => 2016 },
+
+
+
+# ]
+
+
+
+# p laptopOptions[1][:model]
 
 
 
@@ -34,7 +162,35 @@
 
 
 
+# vocabPractice = { :gregarious => "outgoing personality", :wallflower => "shy personality" }
+
+
+
+# vocabPractice[:introvert] = "Derrives energy, often, from solitude"
+
+
+
+# p vocabPractice
+
+
+
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+
+
+# shirt = { :brand => "Prada", :color => "White", :size => "ego" }
+
+
+
+# shirt.each do |element|
+
+
+
+#   p element
+
+
+
+# end
 
 
 
