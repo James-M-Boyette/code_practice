564c041f9b13047ac43ed135fa152ba40f14c688
sou7hernsaint <James.M.84.Boyette@gmail.com>
2021-11-29 16:26:47 -0500

    Add Template version & DDP for this month's Ruby practice

---
+++ 5_Array_Practice/test.js
@@ -0,0 +1,9 @@
+// console.log("Hello, my patient boo!")
+// box1 = "stuff"
+// box2 = "other stuff"
+
+// box3 = box1 + box2
+// console.log(box3)
+
+console.log(`Hello! You know what they say, "Can't pleast them all" ... Please tell me your user name:`)
+userName = gets.chomp
