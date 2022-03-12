a1def5dc4128dd408016d658f54ccb7b833dafef
sou7hernsaint <sou7hernsaint@gmail.com>
2022-03-11 22:01:44 -0500

    JavaScript Arrow Function Practice

---
+++ 7_Functions_Practice/22_3_11_Functions_practice_.js
@@ -0,0 +1,104 @@
+// 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+// If 3 goes in, 6 should come out
+
+// const double = function (input) {
+// double = (input) => {
+//     return input * 2
+// }
+
+// console.log(double(6));
+
+// 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+// "Hi I'm bob" becomes "HI I'M BOB"
+
+// capitalize = (input) => {
+//     return input.toUpperCase()
+// }
+
+// console.log(capitalize(`Hi, I'm Bob!`));
+
+
+// 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+// 10 and 4 go in, 6 comes out
+
+// subtract = (input1, input2) => {
+//     return input1 - input2;
+// }
+
+// console.log(subtract(10, 4));
+
+// 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+// 4 goes in, and 16 comes out
+
+// squared = (input) => {
+//     return input * input;
+// }
+
+// console.log(squared(4));
+
+
+// 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+// 'who' goes in and 'w' comes out
+
+// firstLetter = (input) => {
+//     return input[0]
+// }
+
+// console.log(firstLetter('Who are you'));
+
+
+// 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+// 'who' 'am' 'I' goes in, 'Who am I' comes out
+
+// sentance = (in1, in2, in3) => {
+//     return in1 + ' ' + in2 + ' ' + in3
+// }
+
+// console.log(sentance('Who', 'am', "I"))
+
+// 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+// 3 does in, '3' comes out
+
+// toString = (input) => {
+//     console.log(typeof input)
+//     let string = '';
+//     string += input;
+//     console.log(typeof string)
+//     return string
+// }
+
+// console.log(toString(100));
+
+// 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+// "run" goes in, "run run run run run"
+
+// repeater = (input) => {
+//     let output = '';
+//     for (let i = 0; i < 5; i++) {
+//         output += input + ' ';
+//     }
+//     return output
+// }
+
+// console.log(repeater('run'))
+
+// 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+// 3, 6, 9 goes in, 6 comes out
+
+// average = (in1, in2, in3) => {
+//     return (in1 + in2 + in3) / 3
+// }
+
+// console.log(average(3, 6, 9));
+
+// 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+// 10 goes in, 130 comes out
+
+// dollarThirty = (input) => {
+//     return (input * 10) + 30;
+// }
+
+// console.log(dollarThirty(10));
+
+// SOLUTIONS: https://gist.github.com/peterxjang/6a26d3c698c651dd6e9ccb168d32648c
+
