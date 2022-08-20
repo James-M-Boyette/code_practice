9d959ec2121a9ffce3b1957baea05362e3217de6
sou7hernsaint <James.M.84.Boyette@gmail.com>
2022-08-20 13:22:01 -0400

    Update Converting Data practice
    Date:      Thurs Aug 18 21:13:37 2022 -0400

--- 11_Converting_Data_Practice/22_8_18_Converting_Data_Practice.js
+++ 11_Converting_Data_Practice/22_8_18_Converting_Data_Practice.js
@@ -215,6 +215,8 @@ console.log("singleList", singleList);
 //  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 //     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
 
+//
+
 // 10. Given a hash, create a new hash that has the keys and values switched.
 //     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
 
