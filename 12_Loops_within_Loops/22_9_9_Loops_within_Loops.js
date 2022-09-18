9a8b75e1b1e856535a5d4966fd22b2155dc16867
sou7hernsaint <inquiries@james-boyette.com>
2022-09-17 21:33:38 -0400

    feature: add remaining answers

--- 12_Loops_within_Loops/22_9_9_Loops_within_Loops.js
+++ 12_Loops_within_Loops/22_9_9_Loops_within_Loops.js
@@ -89,6 +89,7 @@ console.log("3) all paired:", pairedUp);
 //    For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
 
 const array4 = [5, -2, 1, -9, -7, 2, 6];
+// [-9, -7]
 
 // So, -9 * -7 = 63
 // We need to evaluate whether the two numbers are the same
@@ -102,15 +103,15 @@ const greatestProduct = (inputArray) => {
   let parentNumbers = [];
 
   for (let i = 0; i < inputArray.length - 1; i++) {
-    console.log("🚀current Element:", i);
+    // console.log("🚀current Element:", i);
     for (let i2 = i + 1; i2 < inputArray.length - 1; i2++) {
       const currentProduct = inputArray[i] * inputArray[i2];
-      console.log("current product:", currentProduct);
+      // console.log("current product:", currentProduct);
       if (currentProduct > maxProduct) {
         maxProduct = currentProduct;
         parentNumbers = [inputArray[i], inputArray[i2]];
       }
-      console.log("max product:", maxProduct);
+      // console.log("max product:", maxProduct);
     }
   }
 
@@ -118,17 +119,29 @@ const greatestProduct = (inputArray) => {
 };
 
 const result4 = greatestProduct(array4);
-console.log("result4:", result4);
+console.log("4) result:", result4);
+
+// Can't figure out how to make this into a hash (see below notes ...)
 
 const greatestProductHashed = (inputArray) => {
-  let hash = {}
+  let hash = {};
   for (let i = 0; i < inputArray.length; i++) {
-    
-    hash[inputArray[i]] = 
+    hash[inputArray[i]];
   }
 };
 
-console.log("result4b:", greatestProductHashed(array4));
+// console.log("result4b:", greatestProductHashed(array4));
+
+// [-9, -1, 2, 9]
+
+// Hashes allow us to look up any input immediately. Therefor, it's useful if we want to know if 'x' exists in an array.
+// How would they help us here?
+// We need to find the maximum sum of any two inputs ...
+// What would our win conditions be?
+//  - Two negative numbers could be the answer
+//  - The two largest positive numbers
+// So we could check the two largest negative numbers, and then to compare them to two largest positive numbers, and take whichever was greater
+// I don't need to know if a specific number exists
 
 // 5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
 //    For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
@@ -137,19 +150,131 @@ console.log("result4b:", greatestProductHashed(array4));
 // It should do this by running through the parent array, one element at a time,
 // ... and reading the child elements
 
+const array5 = [
+  [1, 3],
+  [8, 9],
+  [2, 16],
+];
+
+const totaled = (inputArray) => {
+  return inputArray.reduce((acc, cur) => {
+    // console.log("cur0 & cur1:", cur[0] + cur[1]);
+    acc += cur[0] + cur[1];
+    // console.log("acc:", acc);
+    return acc;
+  }, 0);
+};
+const results5 = totaled(array5);
+
+console.log("5) results:", results5);
+
 // 6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
 //    For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
+
+// So, we want to go through each of the first array's elements, adding said element to each of those in the second, and collecting all sums in a new, third element
+
+// First array
+const array6a = [1, 2];
+const array6b = [6, 7, 8];
+
+const sumsArray = (array1, array2) => {
+  return array1.reduce((accumulator, currentArray1Element) => {
+    for (array2Element of array6b) {
+      accumulator.push(currentArray1Element + array2Element);
+    }
+    return accumulator;
+  }, []);
+};
+
+console.log("6) sums:", sumsArray(array6a, array6b));
 
 // 7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
 //    For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
 
+// So, we want to take one array, and multiply each element of that array by each element of that array: 2 * 2, 2 * 8 and 2 * 3, then 8 * 2 etc
+
+const array7 = [2, 8, 3];
+
+const sums = (inputArray) => {
+  return inputArray.reduce((acc, cur) => {
+    inputArray.forEach((element) => {
+      acc.push(cur * element);
+    });
+    return acc;
+  }, []);
+};
+
+console.log("7) sums:", sums(array7));
+
 // 8. Use a nested loop to find the largest sum of any two different numbers within an array.
 //    For example, [1, 8, 3, 10] becomes 18.
+
+// Add each element with each other element to find the two that produce the largest sum
+
+const array8 = [1, 8, 3, 10];
+
+const maxSum = (inputArray) => {
+  let highestSum = inputArray[0] + inputArray[0];
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    for (let j = i; j < inputArray.length; j++) {
+      const currentTotal = inputArray[i] + inputArray[j];
+      if (highestSum < currentTotal) highestSum = currentTotal;
+    }
+  }
+  return highestSum;
+};
+
+console.log("8) maxSum:", maxSum(array8));
 
 // 9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 //    For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
 
+// Find out if any two numbers in the original array can equal 10
+
+const array9 = [2, 5, 3, 1, 0, 7, 11];
+
+// function equals10(inputArray) {
+const equals10 = (inputArray) => {
+  const hash = {};
+
+  for (let i = 0; i < inputArray.length; i++) {
+    let target = 10 - inputArray[i];
+
+    if (hash[target]) {
+      return [inputArray[i], 10 - inputArray[i]];
+    }
+    hash[inputArray[i]] = true;
+  }
+  return false;
+};
+
+console.log("9) equals10:", equals10(array9));
+
+// What did I learn?
+// I got parts of the set up: I knew I needed to
+//  - declare an hash
+//  - store the current number in the array at the end of the funciton, with a value of 'true'
+//  - that there should be a conditional that checks for something ...
+//  - I thought it was the current number when what we actually want to know if the 'target' exists
+// What did I miss?
+//  - I forgot to return 'false'
+//  - I couldn't quite remember whether the hash declaration and the return statement should occur outside the 'for' loop
+//  - I didn't remember that if the conditional attains, I should return (and therefore return out of the entire loop) the succesful array
+//  - I wasn't quite sure whether or where to use a 'for' loop
+//  - Most importantly, I couldn't remember to decalre a target and to store the "sum" minus the current number in it
+
 //10. Use a nested loop to convert an array of string arrays into a single string.
 //    For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
+
+const array10 = [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]];
+
+const comboString = (inputArray) => {
+  return inputArray.reduce((acc, cur) => {
+    cur.forEach((el) => (acc += el));
+    return acc;
+  }, "");
+};
+
+console.log("10) comboString:", comboString(array10));
 
 //SOLUTIONS: https://gist.github.com/peterxjang/af8985dc4fb07ea14b4bd6ba41cb08f8
