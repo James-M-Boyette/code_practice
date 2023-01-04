83efbc7f6d204fa06bef6f2ca5a7102cd906d7b4
James Boyette <inquiries@james-boyette.com>
2023-01-04 13:39:31 -0500

    add show_me_the_money

---
+++ Actualize_Algorithm_Ladder/Basic String/1_show_me_the_money_23_1_4.js
@@ -0,0 +1,33 @@
+// Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+/** 
+ * Test Case #1:
+        Input: “i hate $ but i love money i know i know im crazy”
+        Output: true  
+ * Test Case #2: 
+        Input: “abcdefghijklmnopqrstuvwxyz”
+        Output: false  
+ */
+
+const string1 = "$"
+const string2 = "i hate $ but i love money i know i know im crazy"
+const string3 = "abcdefghijklmnopqrstuvwxyz"
+
+const dollarPresent = (inputString) => {
+	// Check if '$' exists
+	// console.log('inputString: ', inputString);
+	const elements = inputString.split('')
+	for (el of elements) {
+		if (el == '$') {
+			// console.log('$ found!');
+			return true
+		}
+	}
+	// If nothing found, return 'false'
+    return false
+	// Edge Cases?
+}
+
+console.log('dollarPresent (in string1)? ', dollarPresent(string1));
+console.log('dollarPresent (in string2)? ', dollarPresent(string2));
+console.log('dollarPresent (in string3)? ', dollarPresent(string3));
---
+++ Actualize_Algorithm_Ladder/Basic String/1_show_me_the_money_TEMP.js
@@ -0,0 +1,13 @@
+// Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+/** 
+ * Test Case #1:
+        Input: “i hate $ but i love money i know i know im crazy”
+        Output: true  
+ * Test Case #2: 
+        Input: “abcdefghijklmnopqrstuvwxyz”
+        Output: false  
+ */
+
+const string1 = "i hate $ but i love money i know i know im crazy"
+const string2 = "abcdefghijklmnopqrstuvwxyz"
