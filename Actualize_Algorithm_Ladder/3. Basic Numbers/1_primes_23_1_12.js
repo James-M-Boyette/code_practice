2519d8a51b19cdb7202ada7689f59712e4352e36
James Boyette <inquiries@james-boyette.com>
2023-01-13 16:24:02 -0500

    add fizzbuzz & fibonacci

--- Actualize_Algorithm_Ladder/3. Basic Numbers/1_primes_23_1_12.js
+++ Actualize_Algorithm_Ladder/3. Basic Numbers/1_primes_23_1_12.js
@@ -3,6 +3,19 @@
 /** 
  * Decomp:
     - A prime number is only divisible by itself, and 1
+    - What's a square root? 
+        > The number that, when multiplied by itself ("squared") equals the input
+    - What numbers do we need to account for in our square roots in order to confirm a given input is, in fact, prime?
+        > 1 - All numbers can be divided by "1" ... but 1 is not, itself, a prime number (it does not have *2* factors - only itself)
+        > 2 - If the number is divisible by 2, it is even - and all even numbers are *not* primes
+        > 3 - 
+        > !4 ()
+        > 5 - If a number ends in 5 or 0, it will be divisible by 5 .. and therefore not be a prime
+        > !6 - 
+        > 7 - 
+        > !8 - 
+        > !9 - 
+
     - Modulo doesn't help, probably, because it only tells us if the input, when divided by some other number, has no remainder
     - Are there JS methods that already test for this?
 
@@ -23,3 +36,4 @@ const isPrime = (inputNumber) => {
 }
 
 console.log('is num1 prime? ', isPrime(num1));
+ 
